433717e14bc6b9f2878a3ebf2cc3cff6a6d47094
g17923ka <g17923ka@gm.tsuda.ac.jp>
2019-05-21 12:06:18 +0900

    1〜30までの数字を英訳する

--- src/IntToEng.java
+++ src/IntToEng.java
@@ -21,29 +21,83 @@ public class IntToEng {
 
     static String translateEng(int n) {
     	String s = null;
+    	String on = "one";
+    	String tw = "two";
+    	String th = "three";
+    	String fo = "four";
+    	String fi = "five";
+    	String si = "six";
+    	String se = "seven";
+    	String ei = "eight";
+    	String ni = "nine";
+    	String t1 = "teen";
+    	String t2 = "twenty";
+    	String t3 = "thirty";
+    	String t4 = "forty";
+    	String t5 = "fifty";
     
     	if(n == 0){
     		s = "zero";
 		}if(n == 1){
-			s = "one";
+			s = on;
 		}if(n == 2){
-			s = "two";
+			s = tw;
 		}if(n == 3){
-		s = "three";
+		    s = th;
 		}if(n == 4){
-			s = "four";
+			s = fo;
 		}if(n == 5){
-		s = "five";
+		    s = fi;
 		}if(n == 6){
-			s = "six";
+			s = si;
 		}if(n == 7){
-		s = "seven";
+		    s = se;
 		}if(n == 8){
-			s = "eight";
+			s = ei;
 		}if(n == 9){
-		s = "nine";
+		    s = ni;
 		}if(n == 10){
 			s = "ten";
+		}if(n == 11){
+    		s = "eleven";
+		}if(n == 12){
+			s = "twelve";
+		}if(n == 13){
+			s = "thirteen";
+		}if(n == 14){
+		    s = fo + t1;
+		}if(n == 15){
+			s = "fifteen";
+		}if(n == 16){
+		    s = si + t1;
+		}if(n == 17){
+			s = se + t1;
+		}if(n == 18){
+		    s = "eighteen";
+		}if(n == 19){
+			s = ni + t1;
+		}if(n == 20){
+		    s = t2;
+		}if(n == 21){
+			s = t2 + on;
+		}if(n == 22){
+    		s = t2 + tw;
+		}if(n == 23){
+			s = t2 + th;
+		}if(n == 24){
+			s = t2 + fo;
+		}if(n == 25){
+		    s = t2 + fi;
+		}if(n == 26){
+			s = t2 + si;
+		}if(n == 27){
+		    s = t2 + se;
+		}if(n == 28){
+			s = t2 + ei;
+		}if(n == 29){
+		    s = t2 + ni;
+		}if(n == 30){
+			s = t3;
 		}
         return s;
 
